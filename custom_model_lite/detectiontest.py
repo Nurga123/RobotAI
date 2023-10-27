90acd8766d0f1066049e53c61d978c26cb0c235a
Nurga123 <121932059+Nurga123@users.noreply.github.com>
2023-10-27 16:33:05 +0600

    Update detectiontest.py

--- custom_model_lite/detectiontest.py
+++ custom_model_lite/detectiontest.py
@@ -1,19 +1,3 @@
-﻿######## Webcam Object Detection Using Tensorflow-trained Classifier #########
-#
-# Author: Evan Juras
-# Date: 10/27/19
-# Description: 
-# This program uses a TensorFlow Lite model to perform object detection on a live webcam
-# feed. It draws boxes and scores around the objects of interest in each frame from the
-# webcam. To improve FPS, the webcam object runs in a separate thread from the main program.
-# This script will work with either a Picamera or regular USB webcam.
-#
-# This code is based off the TensorFlow Lite image classification example at:
-# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/examples/python/label_image.py
-#
-# I added my own method of drawing boxes and labels using OpenCV.
-
-# Import packages
 import os
 import argparse
 import cv2
@@ -23,26 +7,20 @@ import time
 from threading import Thread
 import importlib.util
 from flask import Flask, render_template, Response, Request
-# Define VideoStream class to handle streaming of video from webcam in separate processing thread
-# Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+
 class VideoStream:
     """Camera object that controls video streaming from the Picamera"""
     def __init__(self,resolution=(640,480),framerate=30):
-        # Initialize the PiCamera and the camera image stream
         self.stream = cv2.VideoCapture(0)
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
-            
-        # Read first frame from the stream
-        (self.grabbed, self.frame) = self.stream.read()
 
-	# Variable to control when the camera is stopped
+	(self.grabbed, self.frame) = self.stream.read()
         self.stopped = False
 
     def start(self):
-	# Start the thread that reads frames from the video stream
-        Thread(target=self.update,args=()).start()
+	Thread(target=self.update,args=()).start()
         return self
 
     def update(self):
@@ -65,22 +43,6 @@ class VideoStream:
 	# Indicate that the camera and thread should be stopped
         self.stopped = True
 
-# Define and parse input arguments
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
-#                     required=True)
-# parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
-#                     default='detect.tflite')
-# parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
-#                     default='labelmap.txt')
-# parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
-#                     default=0.5)
-# parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
-#                     default='1280x720')
-
-
-# args = parser.parse_args()
-
 app = Flask(__name__)
 
 MODEL_NAME = 'models'
@@ -90,40 +52,25 @@ min_conf_threshold = float(0.5)
 resW, resH = 480, 320
 imW, imH = int(resW), int(resH)
 
-# Import TensorFlow libraries
-# If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
 pkg = importlib.util.find_spec('tflite_runtime')
+
 if pkg:
     from tflite_runtime.interpreter import Interpreter
 else:
     from tensorflow.lite.python.interpreter import Interpreter
 
  
-# Get path to current working directory
 CWD_PATH = os.getcwd()
-
-# Path to .tflite file, which contains the model that is used for object detection
 PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
-
-# Path to label map file
 PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
 
-# Load the label map
 with open(PATH_TO_LABELS, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
 
-# Have to do a weird fix for label map if using the COCO "starter model" from
-# https://www.tensorflow.org/lite/models/object_detection/overview
-# First label is '???', which has to be removed.
-if labels[0] == '???':
-    del(labels[0])
-
-# Load the Tensorflow Lite model.
 interpreter = Interpreter(model_path=PATH_TO_CKPT)
 
 interpreter.allocate_tensors()
 
-# Get model details
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 height = input_details[0]['shape'][1]
@@ -134,8 +81,6 @@ floating_model = (input_details[0]['dtype'] == np.float32)
 input_mean = 127.5
 input_std = 127.5
 
-# Check output layer name to determine if this model was created with TF2 or TF1,
-# because outputs are ordered differently for TF2 and TF1 models
 outname = output_details[0]['name']
 
 if ('StatefulPartitionedCall' in outname): # This is a TF2 model
@@ -143,51 +88,41 @@ if ('StatefulPartitionedCall' in outname): # This is a TF2 model
 else: # This is a TF1 model
     boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
-# Initialize frame rate calculation
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 
 controlX = 0.0
 controlY = 0.0
 
-
-# Initialize video stream
 videostream = VideoStream(resolution=(imW,imH),framerate=30).start()
 time.sleep(1)
 
 #for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
 def getFramesGenerator():
     while True:
-        # Start timer (for calculating frame rate)
         t1 = cv2.getTickCount()
 
         global controlX, controlY
         iSee = False
 
-        # Grab frame from video stream
         frame1 = videostream.read()
 
-        # Acquire frame and resize to expected shape [1xHxWx3]
-        frame = frame1.copy()
+	frame = frame1.copy()
         frame = cv2.resize(frame, (480, 320))
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame_resized = cv2.resize(frame_rgb, (320, 320))
         input_data = np.expand_dims(frame_resized, axis=0)
 
-        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
         if floating_model:
             input_data = (np.float32(input_data) - input_mean) / input_std
 
-        # Perform the actual detection by running the model with the image as input
         interpreter.set_tensor(input_details[0]['index'],input_data)
         interpreter.invoke()
 
-        # Retrieve detection results
         boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
         classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
         scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
 
-        # Loop over all detections and draw detection box if confidence is above minimum threshold
         for i in range(len(scores)):
             if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
 
